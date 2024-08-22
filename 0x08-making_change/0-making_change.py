145e7495490936d3541948b78fcc9ec483d1389b
Mustopha Abdulqadir <mustopha@gmail.com>
2024-08-22 08:36:26 -0700

    Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
+"""A function that determines the fewest number of coins needed
+to meet a given amount of total"""
+
 def makeChange(coins, total):
+    """This function will take a list of coins and use it 
+    to calculate the change the total will require
+    """
     if total <= 0:
         return 0
 
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
-    for coin in coins:
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
-    return dp[total] if dp[total] != float('inf') else -1
+    else:
+        coin = sorted(coins)
+        coin.reverse()
+        counter = 0
+        for i in coin:
+            while(total >= i):
+                counter += 1
+                total -= 1
+        if total == 0:
+            return counter
+        return -1
